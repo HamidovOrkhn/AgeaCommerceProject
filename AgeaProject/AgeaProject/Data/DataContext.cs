638a5bf040783923de4439508d855d302b73066c
orxansenior <orxan.hamidov.orxan.hamidov@mail.ru>
2022-02-11 02:04:12 +0400

    update config

---
+++ AgeaProject/AgeaProject/Data/DataContext.cs
@@ -0,0 +1,46 @@
+﻿using AgeaProject.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AgeaProject.Models;
+
+namespace AgeaProject.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext()
+        {
+
+        }
+
+        public DataContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile("appsettings.json").Build();
+            if (!options.IsConfigured)
+            {
+                options.UseMySQL(configuration.GetConnectionString(nameof(DataContext)));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new CategoryFluent());
+            builder.ApplyConfiguration(new SubCategoryFluent());
+            builder.ApplyConfiguration(new SubCategoryCredentialFluent());
+        }
+
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<SubCategory> SubCategories { get; set; }
+        public DbSet<SubCategoryCredential> SubCategoryCredentials { get; set; }
+
+    }
+}
